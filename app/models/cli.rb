f2c1f7a5e9a3cedb341baaf52ff7d6cdc94c773a
flatiron <flatiron@BERLIN.local>
2019-03-26 09:13:12 +0000

    Few more CLI Fetures

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -5,10 +5,11 @@ class CLI
   def username(sample)
     username_temp = User.find_by name: sample.to_s
     if username_temp
-      puts "Username found, welcome back#{username_temp.name}"
+      @user = username_temp
+      puts "Username found, welcome back #{username_temp.name}!"
     else
       new = User.create(name: sample)
-      puts "Hello #{new.name}"
+      puts "Welcome #{new.name}!"
   end
 end
 
@@ -16,10 +17,23 @@ end
     puts "THIS IS FLIXME!!!!"
 	end
 
+  def show_menu
+    prompt = TTY::Prompt.new
+  choice = prompt.select("What would you like to do", ["Broswe Most Viewed Movies?", "Browse FriendList", "Add new Friend"])
+  if choice == "Add new Friend"
+    #addfriend
+  elsif choice == "Broswe Most Viewed Movies?"
+    #show most viewed movies
+  elsif choice == "Browse FriendList"
+    @user.friendships
+  end
+end
+
 	def start
 	welcome
   prompt = TTY::Prompt.new
   get_username = prompt.ask("What's your username?")
   username(get_username.to_s)
+  show_menu
 	end
 end
